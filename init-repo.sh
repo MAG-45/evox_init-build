61c54a334420b2ac118262065d86965849f677db
MAG <raphvmag@ik.me>
2025-02-22 12:50:06 +0100

    Correct OTA Patch

--- init-repo.sh
+++ init-repo.sh
@@ -17,10 +17,10 @@ git fetch https://github.com/snapboss/android_frameworks_base
 git cherry-pick -Xtheirs 53ebff0802f1043f361c699442a982b5b6e7792a   
 cd ../..
 # Fix OTA
-cp ../0001-Make-OTA-Working-for-unofficial.patch vendor/lineage/0001-Make-OTA-Working-for-unofficial.patch
-cd vendor/lineage
-git am < 0001-Make-OTA-Working-for-unofficial.patch
-cd ../../
+cp ../0001-Change-URL-Strings.patch packages/apps/Updater/0001-Change-URL-Strings.patch
+cd packages/apps/Updater
+git am < 0001-Change-URL-Strings.patch
+cd ../../../
 # Sync repo
 repo sync -c -j$(nproc --all) --force-sync --no-clone-bundle --no-tags
 
