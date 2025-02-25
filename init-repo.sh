8b2123c123de886aa619ff0e34e5c3dca51e67b6
MAG <raphvmag@ik.me>
2025-02-25 19:27:06 +0100

    Update FP Patch

--- init-repo.sh
+++ init-repo.sh
@@ -13,8 +13,8 @@ git clone https://github.com/MAG-45/evox_manifest_duchamp .repo/local_manifests
 sed -i '/<project path="hardware\/lineage\/compat" name="LineageOS\/android_hardware_lineage_compat" \/>/d' .repo/manifests/snippets/lineage.xml
 # Fix FP Framework Issue
 cd frameworks/base
-git fetch https://github.com/snapboss/android_frameworks_base
-git cherry-pick -Xtheirs 53ebff0802f1043f361c699442a982b5b6e7792a   
+wget https://raw.githubusercontent.com/MAG-45/duchamp_patch/refs/heads/vic_evo/fp_evo.patch
+git am < fp_evo.patch
 cd ../..
 # Sync repo
 repo sync -c -j$(nproc --all) --force-sync --no-clone-bundle --no-tags
