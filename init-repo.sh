d3558565e5083120ddc0355901d794f28d1c6b24
MAG <raphvmag@ik.me>
2025-03-04 21:45:55 +0100

    update manifest patch

--- init-repo.sh
+++ init-repo.sh
@@ -11,6 +11,7 @@ mkdir .repo/local_manifests
 git clone https://github.com/MAG-45/evox_manifest_duchamp .repo/local_manifests
 # Fix Hardware lineage manifests
 sed -i '/<project path="hardware\/lineage\/compat" name="LineageOS\/android_hardware_lineage_compat" \/>/d' .repo/manifests/snippets/lineage.xml
+sed -i '/<project path="external\/wpa_supplicant_8" name="LineageOS\/android_external_wpa_supplicant_8" groups="pdk" \/>/d' .repo/manifests/default.xml
 # Fix FP Framework Issue
 cd frameworks/base
 wget https://raw.githubusercontent.com/MAG-45/duchamp_patch/refs/heads/vic_evo/fp_evo.patch
